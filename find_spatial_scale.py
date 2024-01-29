61b4c12165c3c261cce116a9658163e57240055b
James R Young <james@youngbros.com>
2024-01-29 18:56:32 -0500

    adding peak detection from linout to compute FWHM and plasma sheath expansion

--- find_spatial_scale.py
+++ find_spatial_scale.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 calibration_directory = './calibration/'
 im_directory = './calibration/'
 image_to_analyze ='airforce-test-strip-backlit-greenlaser.tif'
-im_directory = './1_19_2024_laser_spark/'
-image_to_analyze ='Sequence45(UBSi121HB2062).tif'
+im_directory = './1_26_2024_laser_spark/'
+image_to_analyze ='Sequence48(UBSi121HB2062).tif'
 path_to_image = im_directory+image_to_analyze
 save_directory = im_directory+image_to_analyze.split('/')[-1].split('.')[0]+'/'
 
@@ -28,8 +28,8 @@ ut.save_parts(directory=save_directory, image_parts=image_parts)
 aligned_images = []
 
 # Compute the spatial mm/pixel
-ld = 218
-dl = 207 
+ld = 779
+dl = 767 
 dpix = ld-dl
 
 group = 2
@@ -41,6 +41,20 @@ mmpll = 1/lppmm
 # Compute the spatial mm/pixel
 pixpmm = dpix*lppmm
 mmppix = 1/pixpmm
+
+fs = 500e6 # samp/sec
+dt = 1/fs # sec/samp
+
+# Compute number of frames 
+num_frames = 12
+
+# Compute time of each frame 
+frame_time = np.arange(0, num_frames, 1)*dt
+
+# Initialize array for tracking width of plasma in time 
+plasma_width = np.zeros_like(frame_time)
+
+# mmppix  = 1
 
 # Load calibration offsets #
 try:
@@ -76,7 +90,7 @@ try:
 
         # Plot the lineout
         n_avg = 10
-        pixel = 700
+        pixel = 550
         lineout = aligned_image[:, pixel-n_avg:pixel+n_avg].mean(axis=1)
         lineout = np.flip(lineout)
         xlineout = np.arange(0, len(lineout), 1)
@@ -88,21 +102,54 @@ try:
 
         # Find the indices of the most extreme inflection points
         extreme_inflection_points = np.where(np.abs(np.diff(np.sign(slope))) > 1)[0]
-        ax3 = ax2.twinx()  # instantiate a second axes that shares the same x-axis
+        # ax3 = ax2.twinx()  # instantiate a second axes that shares the same x-axis
         ax2.plot(lineout, mmppix*xlineout)
         # ax3.plot(slope, range(lineout.size), 'r-')
 
         ax2.set_title('Lineout of Aligned Image')
         ax2.set_ylim(0, mmppix*aligned_image.shape[0])
-        ax3.set_ylim(0, mmppix*aligned_image.shape[0])
+        # ax3.set_ylim(0, mmppix*aligned_image.shape[0])
         aligned_image_marked = aligned_image.copy()
         aligned_image_marked[:, pixel-n_avg:pixel+n_avg] = 0
-        ax1.imshow(aligned_image_marked, aspect="auto", extent=[0,mmppix*colmax,0,mmppix*rowmax])        
+        ax1.imshow(aligned_image_marked, aspect="auto", extent=[0,mmppix*colmax,0,mmppix*rowmax], cmap='Reds')        
+
+        # Find the maximum value of the lineout
+        max_idx = np.argmax(lineout)
+        max_val = lineout[max_idx]
+
+        # Now find the location of the half max of this value
+        half_max = max_val/2
+
+        # Find the indices of the points closest to the half max
+        half_max_idx_top = np.argmin(np.abs(lineout[:max_idx]-half_max))
+        half_max_idx_bottom = np.argmin(np.abs(lineout[max_idx:]-half_max))
+        ax2.plot([half_max, half_max], [mmppix*half_max_idx_top, mmppix*(half_max_idx_bottom+max_idx)], 'k--')
 
         # ax2.set_xlim(0, 255)
         plt.tight_layout()
+        plt.savefig(f'{save_directory}aligned_image_{i}_lineout.png')
         # plt.show()
 
+        # Compute the FWHM
+        fwhm = mmppix*np.abs(half_max_idx_top - max_idx)+mmppix*np.abs(half_max_idx_bottom-max_idx+max_idx)
+        width = mmppix*half_max_idx_top + mmppix*(half_max_idx_bottom+max_idx)
+        print("FWHM: ", fwhm)
+        print("width: ", width)
+        plasma_width[i] = fwhm
+
+    # Plot the plasma width in time
+    plt.figure()
+    plt.plot(10**9*frame_time, plasma_width)
+    plt.xlabel("Time (ns)")
+    plt.ylabel("Plasma Width (mm)")
+
+    # add text box to figure 
+    speed = (1e-3)*(plasma_width[-1]-plasma_width[0])/2/(frame_time[-1]-frame_time[0])
+    textstr = str(np.round(speed/1000, 3))+ "km/s"
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    plt.text(0.05, 0.95, textstr, transform=plt.gca().transAxes, fontsize=14,
+            verticalalignment='top', bbox=props)
+    plt.tight_layout()
     plt.show()
 
 except FileNotFoundError:
