Fixed factor of two in computing widths, added plotting functions for shock width and velocity as a function of time

fit.py
@@ -35,18 +35,18 @@ def plotFits(lineouts,fits,save=False):
         plotFit(fits[i],lineouts[i],i,save)
     plt.show()
 
-def findWidths(fits,mmppix,num_frames):
+def findWidths(fits,mmppix,num_frames,units=0.001):
     widths = np.zeros([num_frames])
     for i in np.linspace(0,num_frames-1,num_frames,dtype='int'):
-        widths[i] = mmppix*fits[i,1][1]/2 #Save Gaussian width, rescale from mm to pixels 
+        widths[i] = units*mmppix*fits[i,1][1] #Save Gaussian width, rescale from mm to pixels 
     return widths
 
-def computeVelocity(widths,units,frame_time,num_frames): #Since widths are in mm, arg "units" is a scaling constant letting the user set the spatial units of the velocity (i.e., for mm/s, use units = 1)
-    vAvg = ((widths[-1]-widths[0])/(frame_time[-1]-frame_time[0]))*units
+def computeVelocity(widths,frame_time,num_frames): #Since widths are in mm, arg "units" is a scaling constant letting the user set the spatial units of the velocity (i.e., for mm/s, use units = 1)
+    vAvg = ((widths[-1]-widths[0])/(frame_time[-1]-frame_time[0]))
     vn = np.zeros([num_frames-1])
     print(f'\n\nWidths: {widths}\n\n')
     for i in np.linspace(1,num_frames-1,num_frames-1,dtype='int'):
-        vn[i-1] = ((widths[i]-widths[i-1])/(frame_time[i]-frame_time[i-1]))*units
+        vn[i-1] = ((widths[i]-widths[i-1])/(frame_time[i]-frame_time[i-1]))
     return vn,vAvg
 
 def plotVn(vn,vAvg,frame_time,units='m/s'):
@@ -54,5 +54,29 @@ def plotVn(vn,vAvg,frame_time,units='m/s'):
     plt.xlabel('Frame Times (s)')
     plt.ylabel(f'Velocities ({units})')
     plt.title('Average Velocity of Shock Between Time Steps')
-    plt.text(1.25e-8,10000,f'Average Velocity: {round(vAvg,2)} ({units})')
+    plt.text(frame_time[1],np.max(vn)*(9.9/10),f'Average Velocity: {round(vAvg,2)} {units}')
+    plt.show()
+
+def plotWidth(widths,frame_time,vAvg,units='m'):
+    plt.plot(frame_time,widths)
+    plt.xlabel('Frame Times (s)')
+    plt.ylabel(f'Widths ({units})')
+    plt.title(f'Spark Width as a Function of Time, Average Velocity = {round(vAvg,2)} {units}/s')
+    plt.show()
+
+def plotCalculations(widths,vn,vAvg,frame_time,units='m'):
+    fig,ax = plt.subplots(1,2)
+    fig.set_size_inches(18, 10, forward=True)
+
+    ax[0].plot(frame_time[1:],vn)
+    ax[0].set_xlabel('Frame Times (s)')
+    ax[0].set_ylabel(f'Velocities ({units}/s)')
+    ax[0].set_title('Average Velocity of Shock')
+    ax[0].text(frame_time[1],np.max(vn)*(9.9/10),f'Average Velocity: {round(vAvg,2)} {units}/s')
+
+    ax[1].plot(frame_time,widths)
+    ax[1].set_xlabel('Frame Times (s)')
+    ax[1].set_ylabel(f'Widths ({units})')
+    ax[1].set_title(f'Spark Width, Average Velocity = {round(vAvg,2)} {units}/s')
+
     plt.show()